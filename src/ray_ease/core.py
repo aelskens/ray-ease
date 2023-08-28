389f08ccfb163679514304bbc86f890338ed83e2
aelskens <arthurelsk@gmail.com>
2023-08-28 14:22:04 +0000

    feat: more practical use of the package
    It is no longer required to ray_ease.init() before the definition of the first decorated function/actor.

--- src/ray_ease/core.py
+++ src/ray_ease/core.py
@@ -10,7 +10,7 @@ from .remote_as_local import remote_actor_as_local
 from .utils import overload
 
 
-def init(*args: Any, config: str = "ray", **kwargs: Any) -> Optional[BaseContext]:
+def init(config: str = "ray", *args: Any, **kwargs: Any) -> Optional[BaseContext]:
     """Wrapper around the `ray.init()` function to specify whether the program should run in a serial or
     in a parallel manner.
 
@@ -26,7 +26,7 @@ def init(*args: Any, config: str = "ray", **kwargs: Any) -> Optional[BaseContext
     if config in ["serial"]:
         os.environ["RAY_EASE"] = config
 
-        return
+        return None
 
     elif config in ["ray"]:
         os.environ["RAY_EASE"] = config
@@ -34,40 +34,21 @@ def init(*args: Any, config: str = "ray", **kwargs: Any) -> Optional[BaseContext
         return ray.init(*args, **kwargs)
 
     else:
-        return
+        return None
 
 
-@overload
-def parallelize(callable_obj: Callable[..., Any], *ray_args: Any, **ray_kwargs: Any) -> Callable[..., Any]:
-    """A decorator designed to wrap the ray.remote decorator. Its purpose is to enable the seamless use of
-    the Ray framework without introducing syntax overhead. When applied to functions and classes, the
-    decorated elements behave as if they were local functions and classes, effectively eliminating the need
-    to deal with Ray's explicit syntax.
-
-    Furthermore, in situations where Ray is not initialized, the program will automatically execute
-    serially, following python default behavior. This means that the decorated functions and classes will
-    be local elements, effectively avoid Ray's overhead which is unnecessary for serial application.
-
-    In the case of decorated class, each member's signature except the dunder ones are modified to contain
-    additionally an optional boolean argument `block` defaulting to False. This argument is used to determine
-    whether ray.get should be used when calling the method or not (for more information and illustration refer
-    to ray_ease.remote_as_local.RemoteActorAsLocal implementation).
-
-    Additionally, the ray.remote args and kwargs can be provided to this decorator and are used when calling
-    ray.remote.
-
-    :param callable_obj: Either a function or a class to parallelize with the Ray framework.
-    :type callable_obj: Callable[..., Any]
-    :raises TypeError: Raised if the decorated object is not a callable.
-    :return: The decorated callable (remote function or actor).
-    :rtype: Callable[..., Any]
+def _parallelize(callable_obj: Callable[..., Any], *ray_args: Any, **ray_kwargs: Any) -> Callable[..., Any]:
+    """This represents the current operational core encasing the ray.remote decorator. To ensure seamless
+    utilization, given its dependence on whether ray initialization has occurred, this core function is
+    enveloped by another layer. This added layer serves to defer the instantiation of the decorated
+    callable_obj until the moment of its invocation.
     """
 
     if not callable(callable_obj):
         raise TypeError(f"The decorated object should be a callable, not of type: {type(callable_obj)}.")
 
     # Avoid overhead if Ray is not initialized
-    if not ray.is_initialized():
+    if os.getenv("RAY_EASE") in [None, "serial"]:
         return callable_obj
 
     ray_remote = ray.remote
@@ -99,6 +80,56 @@ def parallelize(callable_obj: Callable[..., Any], *ray_args: Any, **ray_kwargs: 
     return _Wrapper(callable_obj)
 
 
+@overload
+def parallelize(callable_obj: Callable[..., Any], *ray_args: Any, **ray_kwargs: Any) -> Callable[..., Any]:
+    """A decorator designed to wrap the ray.remote decorator. Its purpose is to enable the seamless use of
+    the Ray framework without introducing syntax overhead. When applied to functions and classes, the
+    decorated elements behave as if they were local functions and classes, effectively eliminating the need
+    to deal with Ray's explicit syntax.
+
+    Furthermore, in situations where Ray is not initialized, the program will automatically execute
+    serially, following python default behavior. This means that the decorated functions and classes will
+    be local elements, effectively avoid Ray's overhead which is unnecessary for serial application.
+
+    In the case of decorated class, each member's signature except the dunder ones are modified to contain
+    additionally an optional boolean argument `block` defaulting to False. This argument is used to determine
+    whether ray.get should be used when calling the method or not (for more information and illustration refer
+    to ray_ease.remote_as_local.RemoteActorAsLocal implementation).
+
+    Additionally, the ray.remote args and kwargs can be provided to this decorator and are used when calling
+    ray.remote.
+
+    :param callable_obj: Either a function or a class to parallelize with the Ray framework.
+    :type callable_obj: Callable[..., Any]
+    :raises TypeError: Raised if the decorated object is not a callable.
+    :return: The decorated callable (remote function or actor).
+    :rtype: Callable[..., Any]
+    """
+
+    class _Wrapper:
+        def __init__(self, callable_obj: Callable[..., Any], *args: Any, **kwargs: Any) -> None:
+            self.callable_obj = callable_obj
+            self.args = args
+            self.kwargs = kwargs
+
+            self.memoization = {}
+
+        def __call__(self, *usage_args: Any, **usage_kwargs: Any) -> Any:
+            key = self.args + (self.callable_obj,) + tuple(sorted(self.kwargs.items()))
+
+            if key in self.memoization:
+                usable_callable_obj = self.memoization[key]
+
+                return usable_callable_obj(*usage_args, **usage_kwargs)
+
+            self.memoization[key] = _parallelize(self.callable_obj, *self.args, **self.kwargs)
+            usable_callable_obj = self.memoization[key]
+
+            return usable_callable_obj(*usage_args, **usage_kwargs)
+
+    return _Wrapper(callable_obj, *ray_args, **ray_kwargs)
+
+
 def retrieve_parallel_loop(
     loop: Iterable[Any], parallel_progress: bool = False, parallel_progress_kwargs: Dict[str, Any] = {}
 ) -> Iterable[Any]:
@@ -118,7 +149,7 @@ def retrieve_parallel_loop(
     :rtype: Iterable[Any]
     """
 
-    if ray.is_initialized():
+    if os.getenv("RAY_EASE") in ["ray"]:
         if parallel_progress:
             # Remove eventual total key-value because automatically computed hereunder
             parallel_progress_kwargs.pop("total", None)
